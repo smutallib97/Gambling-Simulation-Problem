975f6b0981f398fc3dd511f36fbd1dc8054070b9
smutallib97 <smutallib97@gmail.com>
2022-10-06 10:29:50 +0530

    Added: Welcome to Gambling Simulation Program

---
+++ Gambling_Simulator.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class Gambling_Simulator {
+    
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambling Simulation Program");
+    }
+}
