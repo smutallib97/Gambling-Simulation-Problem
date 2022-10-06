de6e15876e6c5adce5ac0d754821116796ba45d5
smutallib97 <smutallib97@gmail.com>
2022-10-06 10:46:40 +0530

    Added: Gambler make 1 dolar bet so either win or loose

--- Gambling_Simulator.java
+++ Gambling_Simulator.java
@@ -1,13 +1,35 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class Gambling_Simulator {
 
     // UC1 - As a Gambler, would start with a stake of $100 every day and bet $1 every game.
 
     public static final int STAKE = 100;
     public static final int BET_VALUE = 1;
-    
-    public static void main(String[] args) {
-        System.out.println("Welcome to Gambling Simulation Program");
+
+    // UC2 - As a Gambler make $1 bet so either win or loose $1
+
+    public static void winLoose(int r, int BET_VALUE){
+
+        if (r == BET_VALUE){
+            System.out.println("Gambler Wins!");
+        }
+
+        else{
+            System.out.println("Gambler Loose!");
+
+        }
     }
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Gambling Simulation Program.");
+
+        Random rand = new Random();
+        int r = rand.nextInt(2);
+        winLoose(r,BET_VALUE);
+    }
+}
 }
