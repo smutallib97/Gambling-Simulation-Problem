7f91764dd4306eb8c0a009bdff9cc28b82b71df4
smutallib97 <smutallib97@gmail.com>
2022-10-06 10:40:11 +0530

    Added: Gambler start with a stake of 100 dolars and bet 1 dolar

--- Gambling_Simulator.java
+++ Gambling_Simulator.java
@@ -1,6 +1,11 @@
 package com.bridgelabz;
 
 public class Gambling_Simulator {
+
+    // UC1 - As a Gambler, would start with a stake of $100 every day and bet $1 every game.
+
+    public static final int STAKE = 100;
+    public static final int BET_VALUE = 1;
     
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulation Program");
